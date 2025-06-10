af22c911c5cd66eb58565494c756dab045d02b3f
David Ochsner <30530196+ochsnerd@users.noreply.github.com>
2025-06-10 20:40:12 +0200

    Planeseats

--- build.el
+++ build.el
@@ -1,29 +1,3 @@
-;; (require 'ox-publish)
-;; (require 'htmlize)
-
-;; (setq org-html-htmlize-output-type 'css)
-
-;; (setq org-publish-project-alist
-;;       (list
-;;        (list "org-site:main"
-;;              :recursive t
-;;              :base-directory "./content"
-;;              :publishing-function 'org-html-publish-to-html
-;;              :publishing-directory "./public"
-;;              :with-author nil
-;;              :with-creator t
-;;              :with-toc t
-;;              :section-numbers nil
-;;              :time-stamp-file nil
-;;              :html-head-include-default-style t
-;;              :html-head-include-scripts t)))
-
-;; (setq org-html-validation-link nil)
-
-;; ;; Generate the site output
-;; (org-publish-all t)
-
-;; (message "Build complete!")
 (require 'ox-publish)
 (require 'htmlize)
 
